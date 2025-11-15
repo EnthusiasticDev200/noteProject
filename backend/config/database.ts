72da32b774c352e0428df82c1011d3bdb506094b
EnthusiasticDev200 <joshuamakanjuola99@gmail.com>
2025-11-15 17:54:07 +0100

    modified file

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,4 +1,4 @@
-import { MongoClient} from "mongodb"
+import { MongoClient, Db, Collection} from "mongodb"
 import dotenv from "dotenv"
 
 dotenv.config()
@@ -6,31 +6,40 @@ dotenv.config()
 const dbName = process.env.MONGODB_NAME!
 const uri = process.env.MONGODB_STRING! //local
 
-const db = async() =>{
-    const client = new MongoClient(uri)
+let db : Db
+
+export const connectDb = async() =>{
     try{
+        if (db) return db // halt reconnection
+        const client = new MongoClient(uri)
+   
         await client.connect()
         console.log('MongoDB connected successfully')
+
+        db = client.db(dbName)
     }catch(error){
         console.error("DB connection failed", error)
     }
+    return db
+}
+
+// create collection
+
+export const userCollection = async() =>{
+    const database = await connectDb()
+    database.collection("users") // create user's collection
+}
+
+
+export const notesCollection = async() =>{
+    const database = await connectDb()
+    database.collection("notes") // create note's collection
+}
+
+
+export const ordersCollection = async() =>{
+    const database = await connectDb()
+    database.collection("orders") // create orders's collection
     
 }
 
-export default db
-
-// try{
-//     if (process.env.NODE_ENV === 'development'){
-//         const uri = process.env.MONGODB_STRING!
-//         client = new MongoClient(uri)
-//         client.connect()
-//         console.log('MongoDB connected successfully')
-//     }
-// }catch(error){
-//     console.log("DB connection failed", error)
-// }
-
-// const db = client?.db('note_project')
-// const users = db?.collection("users")
-// const orders = db?.collection("orders")
-// const notes = db?.collection("notes")
